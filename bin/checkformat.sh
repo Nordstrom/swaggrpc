dd6824dfccaa247b01c9302f996db8489ea9a8f1
Jesse Kinkead <jesse.kinkead@nordstrom.com>
2017-10-17 09:06:07 -0700

    Replace tabs with spaces in shell script.

--- bin/checkformat.sh
+++ bin/checkformat.sh
@@ -3,9 +3,9 @@
 # Check for valid formatting. Print unformatted files & exit with error, if there are any.
 MALFORMED_FILES=(`find . -name '*.go' -type f -not -path '*/vendor/*' -exec gofmt -l '{}' ';'`)
 if [[ -n "$MALFORMED_FILES" ]]; then
-	echo "gofmt must be run on file(s):"
+  echo "gofmt must be run on file(s):"
   for file in ${MALFORMED_FILES[@]}; do
     echo "  $file"
   done
-	exit 1
+  exit 1
 fi
